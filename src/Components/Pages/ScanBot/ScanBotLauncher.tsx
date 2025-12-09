272c57ecc2617f0404bc9742370abff921ab7904
Maxwell Austensen <m.austensen@gmail.com>
2025-12-09 17:18:00 -0500

    Revert "hardcode temp key"
    This reverts commit 32701799e1060f1e67320848747f8164d65e603b.

--- src/Components/Pages/ScanBot/ScanBotLauncher.tsx
+++ src/Components/Pages/ScanBot/ScanBotLauncher.tsx
@@ -5,30 +5,13 @@ import { Button } from "@justfixnyc/component-library";
 
 import { runDocumentScanner } from "./StartDocumentScanner";
 
-const LICENSE_KEY =
-  "haBga9kXT5ZGPXrUuDvZfdrJdz8Oft" +
-  "ap5N3M61rcApGfJCMgmiTirJdMpRvf" +
-  "P5ysX/xCF4knUEDF+MyXmeiL08iz3w" +
-  "5clQqyDHsH9WnV/6CvbC6qGqNtfYSI" +
-  "v6b34vy9qFmC1UV6sBut3V9Fd9w4U/" +
-  "tOMrgrf3MxSqoNBzYKzG41F/+ZPH+k" +
-  "e6AdORRFHbVy/DvISCn7YbKonHLE0l" +
-  "2vl7mgln8KO0O6+s55YlhZpu6mW9T6" +
-  "7PleCWQtGkbiRc1+TMVGKr8H0S31HM" +
-  "78dVNKQxRN25GRMZC9WHK3+5yoXe0t" +
-  "Gz2+VWoP8rbQvwNPoXMiCiNjQH53j6" +
-  "6VTR2xCWL4EA==\nU2NhbmJvdFNESw" +
-  "psb2NhbGhvc3R8anVzdGZpeC5vcmcK" +
-  "MTc2NTkyOTU5OQo4Mzg4NjA3Cjg=\n";
-
 export const ScanBotLauncher: React.FC = () => {
   const [sdkLoaded, setSdkLoaded] = useState(false);
 
   useEffect(() => {
     async function init() {
       await ScanbotSDK.initialize({
-        licenseKey: LICENSE_KEY,
-        // licenseKey: import.meta.env.VITE_SCANBOT_LICENSE_KEY || "",
+        licenseKey: import.meta.env.VITE_SCANBOT_LICENSE_KEY || "",
         /**
          * We have designated a custom path for the wasm file in the public folder.
          * This also means wasm binaries are copied from ScanbotSDK's node_modules to the wasm folder.
