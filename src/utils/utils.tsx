5c8d2fa421419fa58dafb8f508f3b1a24d7c7413
Maxwell Austensen <m.austensen@gmail.com>
2026-01-08 12:55:17 -0500

    prettier

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -1,1 +1,2 @@
-export const getRandomID = (): string => (Math.random() * 10000000).toFixed(0).toString();
+export const getRandomID = (): string =>
+  (Math.random() * 10000000).toFixed(0).toString();
